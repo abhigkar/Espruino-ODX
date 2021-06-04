c82e01677a692f5240111a57a4ff06c80fa3990b
abhinav.golwalkar@gmail.com <abhinav.golwalkar@gmail.com>
2021-06-04 15:27:46 +0530

    added time stamp in CSV file

--- src/storage.js
+++ src/storage.js
@@ -1,27 +1,33 @@
 var isStorageHasData = false;
 
-function addSensorDataToStorage(data){
-    if(data != undefined){
-	    console.log(`,${data[0]},${data[1]},${data[2]},${data[3]},${data[4]}\n`);
-        localStorage.csv += `,${data[0]},${data[1]},${data[2]},${data[3]},${data[4]}\n`;
+
+Date.prototype.toTimestamp = function () {
+    let d = new Date();
+    return `${d.getDate()}${d.getMonth()}${d.getFullYear()}${d.getHours()}${d.getMinutes()}${d.getSeconds()}${d.getMilliseconds()}`;
+}
+
+function addSensorDataToStorage(data) {
+    if (data != undefined) {
+        if (localStorage.csv == undefined || localStorage.csv == "") {
+            localStorage.csv = ",,,,,,\n";
+        }
+        localStorage.csv += `${new Date().toTimestamp()},${data[0]},${data[1]},${data[2]},${data[3]},${data[4]}\n`;
         isStorageHasData = true;
     }
 }
 
-function generateCsvFromStorage(){
-    if(isStorageHasData){
+function generateCsvFromStorage() {
+    if (isStorageHasData) {
         var alink = document.createElement("a");
-        //alink.style.display = 'block';
-        alink.href=URL.createObjectURL(new Blob([localStorage.getItem("csv")],{type:"text/csv"}));;
+        alink.href = URL.createObjectURL(new Blob([localStorage.getItem("csv")], { type: "text/csv" }));;
         alink.download = getFileName();
         isStorageHasData = false;
         alink.click();
-        localStorage.csv="";
+        localStorage.csv = "";
         alink.remove();
     }
 }
 
-function getFileName(){
-    let d = new Date();
-    return `${d.getDate()}${d.getMonth()}${d.getFullYear()}${d.getHours()}${d.getMinutes()}${d.getSeconds()}.csv`;
+function getFileName() {
+    return new Date().toTimestamp() + ".csv";
 }
